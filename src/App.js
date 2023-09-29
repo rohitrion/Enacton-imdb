6198bbed7f98bbfd6a70614790bf40a59c6b9170
MadhuSaini22 <madhusaini46810@gmail.com>
2023-09-29 19:05:48 +0530

    29updates

--- src/App.js
+++ src/App.js
@@ -1,60 +1,3 @@
-// import "./App.css";
-// import React from "react";
-// import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
-// import Home from "./pages/Home";
-// import Navbar from "./Component/Navbar";
-// import Singlemovie from "./pages/Singlemovie";
-// import Watchlist from "./pages/Watchlist";
-// import Login from "./Firebase/Login";
-// import Signup from "./Firebase/Signup";
-// import { useEffect, useState } from "react";
-// import { auth } from "./Firebase/firebase";
-// import { useRecoilState } from "recoil";
-// import { login } from "./recoil";
-// import Error from "./pages/Error";
-// import { Layout } from "./Layout";
-// import { Footer, routes } from "./Config/routes";
-
-// function App() {
-//   const [username, setUsername] = useState("");
-
-//   const [log, setLog] = useRecoilState(login);
-
-//   useEffect(() => {
-//     auth.onAuthStateChanged((user) => {
-//       if (user) {
-//         setUsername(user.displayName);
-//         setLog(true);
-//       } else {
-//         setUsername("");
-//         setLog(false);
-//       }
-//     });
-//   }, []);
-
-//   return (
-//     <>
-
-//       <BrowserRouter>
-       
-//         <Routes>
-//         {routes.map((route, index) => {
-//         if (
-//           ( routes.islogin && log) 
-//         ) {
-//           return <Route key={index} path={route.path} element={route.element} />;
-//         }
-//         return null;
-//       })}
-//         </Routes>
-
-//       </BrowserRouter>
-//     </>
-//   );
-// }
-
-// export default App;
-
 
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
@@ -72,9 +15,8 @@ import { Layout } from "./Layout";
 import { Footer, routes } from "./Config/routes";
 
 function App() {
-  
   const [log, setLog] = useRecoilState(login);
- const [name,setname]=useRecoilState(Name)
+  const [name, setname] = useRecoilState(Name);
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       if (user) {
@@ -109,7 +51,6 @@ function App() {
             />
           ))}
         </Routes>
-    
       </BrowserRouter>
     </>
   );
--- src/Component/Utils/icons.jsx
+++ src/Component/Utils/icons.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 export const PlusIcon = () => {
   return (
-
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
@@ -17,13 +16,11 @@ export const PlusIcon = () => {
         d="M12 4.5v15m7.5-7.5h-15"
       ></path>
     </svg>
-
   );
 };
 
 export const Rel = () => {
   return (
-
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
@@ -38,15 +35,11 @@ export const Rel = () => {
         d="M12 4.5v15m7.5-7.5h-15"
       ></path>
     </svg>
-
   );
 };
 
-
-
 export const Menubar = () => {
   return (
-
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="24"
@@ -59,15 +52,11 @@ export const Menubar = () => {
       <path fill="none" d="M0 0h24v24H0V0z"></path>
       <path d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zm0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1z"></path>
     </svg>
-
   );
 };
 
-
-
 export const Dropdown = () => {
   return (
-
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="24"
@@ -80,16 +69,11 @@ export const Dropdown = () => {
       <path fill="none" d="M0 0h24v24H0V0z"></path>
       <path d="M8.71 11.71l2.59 2.59c.39.39 1.02.39 1.41 0l2.59-2.59c.63-.63.18-1.71-.71-1.71H9.41c-.89 0-1.33 1.08-.7 1.71z"></path>
     </svg>
-
   );
 };
 
-
-
-
 export const Serachicon = () => {
   return (
-
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="24"
@@ -102,16 +86,11 @@ export const Serachicon = () => {
       <path fill="none" d="M0 0h24v24H0V0z"></path>
       <path d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0 .41-.41.41-1.08 0-1.49L15.5 14zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
     </svg>
-
   );
 };
 
-
-
-
 export const Watchlist = () => {
   return (
-
     <svg
       width="24"
       height="24"
@@ -126,81 +105,63 @@ export const Watchlist = () => {
         fill="currentColor"
       ></path>
     </svg>
-
   );
 };
-
-
-
 
 export const Loginicon = () => {
   return (
-
     <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    class="ipc-icon ipc-icon--account-circle ipc-btn__icon ipc-btn__icon--pre"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    role="presentation"
-  >
-    <path fill="none" d="M0 0h24v24H0V0z"></path>
-    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2a7.2 7.2 0 01-6-3.22c.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08a7.2 7.2 0 01-6 3.22z"></path>
-  </svg>
-
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      class="ipc-icon ipc-icon--account-circle ipc-btn__icon ipc-btn__icon--pre"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      role="presentation"
+    >
+      <path fill="none" d="M0 0h24v24H0V0z"></path>
+      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2a7.2 7.2 0 01-6-3.22c.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08a7.2 7.2 0 01-6 3.22z"></path>
+    </svg>
   );
 };
-
 
 export const Whitedropdown = () => {
   return (
-
     <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    class="ipc-icon ipc-icon--arrow-drop-down ipc-btn__icon ipc-btn__icon--post navbar__flyout__text-button-post-icon"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    role="presentation"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      class="ipc-icon ipc-icon--arrow-drop-down ipc-btn__icon ipc-btn__icon--post navbar__flyout__text-button-post-icon"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      role="presentation"
     >
-    <path fill="none" d="M0 0h24v24H0V0z"></path>
-    <path d="M8.71 11.71l2.59 2.59c.39.39 1.02.39 1.41 0l2.59-2.59c.63-.63.18-1.71-.71-1.71H9.41c-.89 0-1.33 1.08-.7 1.71z"></path>
+      <path fill="none" d="M0 0h24v24H0V0z"></path>
+      <path d="M8.71 11.71l2.59 2.59c.39.39 1.02.39 1.41 0l2.59-2.59c.63-.63.18-1.71-.71-1.71H9.41c-.89 0-1.33 1.08-.7 1.71z"></path>
     </svg>
-
   );
 };
-
-
-
 
 export const Videoicon = () => {
   return (
-
     <svg
-    width="32"
-    height="32"
-    xmlns="http://www.w3.org/2000/svg"
-    class="group-hover:cursor-pointer group-hover:text-yellow-default ipc-icon ipc-icon--play-circle-outline-large-inline ipc-icon--inline sc-d4cb23a2-14 ejjmJF editorial-play-icon'
+      width="32"
+      height="32"
+      xmlns="http://www.w3.org/2000/svg"
+      class="group-hover:cursor-pointer group-hover:text-yellow-default ipc-icon ipc-icon--play-circle-outline-large-inline ipc-icon--inline sc-d4cb23a2-14 ejjmJF editorial-play-icon'
 id='iconContext-play-circle-outline-large-inline"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    role="presentation"
-  >
-    <path d="M10.803 15.932l4.688-3.513a.498.498 0 0 0 0-.803l-4.688-3.514a.502.502 0 0 0-.803.402v7.026c0 .412.472.653.803.402z"></path>
-    <path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-1c6.075 0 11-4.925 11-11S18.075 1 12 1 1 5.925 1 12s4.925 11 11 11z"></path>
-  </svg>
-
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      role="presentation"
+    >
+      <path d="M10.803 15.932l4.688-3.513a.498.498 0 0 0 0-.803l-4.688-3.514a.502.502 0 0 0-.803.402v7.026c0 .412.472.653.803.402z"></path>
+      <path d="M12 24C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12zm0-1c6.075 0 11-4.925 11-11S18.075 1 12 1 1 5.925 1 12s4.925 11 11 11z"></path>
+    </svg>
   );
 };
 
-
-
-
 export const Imdb = () => {
   return (
-
     <svg
       id="home_img"
       class="ipc-logo"
@@ -224,7 +185,5 @@ export const Imdb = () => {
         <path d="M44.4299079,4.50685823 L44.749518,4.50685823 C46.5447098,4.50685823 48,5.91267586 48,7.64486762 L48,14.8619906 C48,16.5950653 46.5451816,18 44.749518,18 L44.4299079,18 C43.3314617,18 42.3602746,17.4736618 41.7718697,16.6682739 L41.4838962,17.7687785 L37,17.7687785 L37,0 L41.7843263,0 L41.7843263,5.78053556 C42.4024982,5.01015739 43.3551514,4.50685823 44.4299079,4.50685823 Z M43.4055679,13.2842155 L43.4055679,9.01907814 C43.4055679,8.31433946 43.3603268,7.85185468 43.2660746,7.63896485 C43.1718224,7.42607505 42.7955881,7.2893916 42.5316822,7.2893916 C42.267776,7.2893916 41.8607934,7.40047379 41.7816216,7.58767002 L41.7816216,9.01907814 L41.7816216,13.4207851 L41.7816216,14.8074788 C41.8721037,15.0130276 42.2602358,15.1274059 42.5316822,15.1274059 C42.8031285,15.1274059 43.1982131,15.0166981 43.281155,14.8074788 C43.3640968,14.5982595 43.4055679,14.0880581 43.4055679,13.2842155 Z"></path>
       </g>
     </svg>
-
-
-  )
-}
+  );
+};
